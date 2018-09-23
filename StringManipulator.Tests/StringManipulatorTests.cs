20138b67ce7f02cd464b887b89f2343c1c635a54
Denys Poliakov <denys.poliakov@noobaa.com>
2018-09-23 18:59:07 +0300

    initial

---
+++ StringManipulator.Tests/StringManipulatorTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using StringManipulator;
+
+namespace StringManipulator.Tests
+{
+    [TestFixture]
+    public class StringManipulatorTests
+    {
+        StringManipulator sut = new StringManipulator();
+        [Test]
+        public void ShouldRevertString()
+        {
+            var result = sut.Revert("abcd");
+            Assert.That(result, Is.EqualTo("dcba"));
+        }
+        [TestCase("aBCD", "Abcd")]
+        [TestCase("", "")]
+        [TestCase("123", "123")]
+        [TestCase("@//ТеСТ", "@//тест")]
+        public void ShouldFixCaps(string test, string expected)
+        {
+            var result = sut.CapsFix(test);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+
+}
---
+++ StringManipulator/StringManipulator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StringManipulator
+{
+    public class StringManipulator
+    {
+        public string Revert(string S)
+        {
+            string result = "";
+            for (int i = 0; i < S.Length; i++)
+            {
+                result += S[S.Length - 1 - i];
+            }
+            return result;
+        }
+        public string CapsFix(string S)
+        {
+            if (String.Equals(S, S.ToUpper())) 
+            {
+                return S;
+            }
+            else 
+            {
+                return S[0].ToString().ToUpper() + S.Substring(1).ToLower(); 
+            }
+        }
+    }
+}
